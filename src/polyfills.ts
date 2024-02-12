adf5d0f150ac3600773595642412c826fc5b2717
lauqch <lauraqch21@gmail.com>
2024-02-11 20:08:33 -0400

    bootcamp

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+/*eslint-disable @typescript-eslint/no/splicit/any*/
+import { Buffer } from 'buffer'; 
+
+(window as any).global = window;
+(window as any).global.Buffer = Buffer;
+(window as any).process = { env: { } };
